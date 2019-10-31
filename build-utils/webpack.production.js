549c4aaed6b3e8e72141a9e5f384ced971696a54
Jeff Schomay <jschomay@gmail.com>
2019-10-31 09:27:05 -0700

    remove css purifying
    it's getting rid of some things I want and it isn't saving that much
    space anyway

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const PurifyCSSPlugin = require('purifycss-webpack');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -24,8 +24,8 @@ module.exports = () => ({
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'postcss-loader',
-        ],
+          'postcss-loader'
+        ]
       }
     ]
   },
@@ -33,34 +33,34 @@ module.exports = () => ({
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'assets/css/[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
+      chunkFilename: '[id].[hash].css'
     }),
 
-    new PurifyCSSPlugin({
-      // Give paths to parse for rules. These should be absolute!
-      paths: glob.sync(path.join(__dirname, '../src/**/*.elm')),
-      verbose: true,
-    }),
+    // new PurifyCSSPlugin({
+    //   // Give paths to parse for rules. These should be absolute!
+    //   paths: glob.sync(path.join(__dirname, '../src/**/*.elm')),
+    //   verbose: true,
+    // }),
 
     new CopyWebpackPlugin([
-      { from: 'src/img', to: 'img/' },
+      {from: 'src/img', to: 'img/'}
     ]),
 
-    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+    new ImageminPlugin({test: /\.(jpe?g|png|gif|svg)$/i}),
 
     new UglifyJsPlugin({
       cache: true,
       parallel: true,
       uglifyOptions: {
         compress: {
-          pure_funcs: ['F2','F3','F4','F5','F6','F7','F8','F9','A2','A3','A4','A5','A6','A7','A8','A9'],
+          pure_funcs: ['F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9'],
           pure_getters: true,
           keep_fargs: false,
           unsafe_comps: true,
           unsafe: true,
-          passes: 3,
-        },
-      },
+          passes: 3
+        }
+      }
     }),
 
     new OptimizeCSSAssetsPlugin()
