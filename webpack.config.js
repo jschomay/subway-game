b49d415e51ff233dfbad6c3819d7763fd09ba1c0
Jeff Schomay <jschomay@gmail.com>
2023-03-10 11:20:10 -0500

    copy over all new start configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const webpack = require("webpack");
-const webpackMerge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const StyleLintPlugin = require("stylelint-webpack-plugin");
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 const presetConfig = require("./build-utils/loadPresets");
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
@@ -14,49 +14,28 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
     {
       mode,
 
-      resolve: {
-        alias: {}
-      },
-
       entry: {
-        index: "./src/index.js"
-      },
-
-      module: {
-        noParse: /\.elm$/,
-        rules: [
-          {
-            test: /\.(eot|ttf|woff|woff2|svg)$/,
-            use: "file-loader?publicPath=../../&name=fonts/[name].[ext]"
-          },
-          {
-            test: /\.(jpg|png)$/,
-            use: "file-loader?publicPath=../../&name=img/[name].[ext]"
-          }
-        ]
+        main: path.join(__dirname, './src/index.js'),
+        vendor: path.join(__dirname, './src/assets/js/vendor.js'),
       },
 
       plugins: [
         new HtmlWebpackPlugin({
-          template: "src/index.html",
-          inject: "body",
-          chunks: ["index"],
-          filename: "index.html"
-        }),
-        new HtmlWebpackPlugin({
-          template: "src/about.html",
-          filename: "about.html",
-          chunks: []
+          template: 'src/assets/index.html',
+          inject: 'body',
+          filename: 'index.html',
         }),
 
         new StyleLintPlugin(),
 
-        new CopyWebpackPlugin([
-          { from: "src/screenshots/", to: "screenshots/" }
-        ])
+        new CopyWebpackPlugin({
+          patterns: [
+            { from: 'src/assets/favicon.ico' }
+          ]
+        }),
       ]
     },
     modeConfig(mode),
-    presetConfig({ mode, presets })
-  );
+    presetConfig({ mode, presets }),
+  )
 };
